7759e16ca394dd20670e665c9460c752eb485ee3
Jan Dytrych <info@johnyseven.cz>
2022-04-02 23:58:55 +0200

    DynamicGauge - remove padding and remove border - add support for changing color, minimum, maximum that defines range of gauge to show - add property text-color to change color of value text - fixed text value number of decimals from binding

--- src/ui/dynamic_gauge.cpp
+++ src/ui/dynamic_gauge.cpp
@@ -22,17 +22,30 @@ void DynamicGauge::load(const JsonObject &json)
     lv_arc_set_bg_angles(arc, 0, 300);
     lv_arc_set_angles(arc, 0, 300);
     lv_arc_set_rotation(arc, 120);
-    lv_arc_set_value(arc, 0);
-
-    lv_color_t colors[1];
-    colors[0] = lv_theme_get_color_primary();
+    lv_arc_set_value(arc, 20);
+    lv_arc_set_adjustable(arc, false);
+    lv_obj_set_click(arc, false);
+    //remove border
+    lv_obj_set_style_local_pad_all(arc, LV_ARC_PART_BG, LV_STATE_DEFAULT, 0);
+    lv_obj_set_style_local_border_side(arc, LV_ARC_PART_BG, LV_STATE_DEFAULT, LV_BORDER_SIDE_NONE);
+    //set opacity to transparent
+    lv_obj_set_style_local_bg_opa(arc, LV_ARC_PART_BG, LV_STATE_DEFAULT, LV_OPA_TRANSP);
 
     if (json.containsKey("color"))
     {
-        colors[0] = DynamicHelpers::get_color(json["color"]);
+        auto color = DynamicHelpers::get_color(json["color"]);
+        lv_obj_set_style_local_line_color(arc, LV_ARC_PART_INDIC, LV_STATE_DEFAULT, color);
     }
 
-    lv_obj_set_style_local_line_color(arc, LV_ARC_PART_INDIC, LV_STATE_DEFAULT, colors[0]);
+    if(json.containsKey("minimum"))
+    {
+        minimum_ = json["minimum"].as<float>();
+    }
+
+    if(json.containsKey("maximum"))
+    {
+        maximum_ = json["maximum"].as<float>();
+    }
 
     if (json.containsKey("binding"))
     {
@@ -79,6 +92,13 @@ void DynamicGauge::load(const JsonObject &json)
     label_ = lv_label_create(arc, NULL);
     lv_label_set_text(label_, "--");
     lv_obj_align(label_, arc, LV_ALIGN_CENTER, 0, 0);
+
+    if(json.containsKey("text-color"))
+    {
+        auto textColor = DynamicHelpers::get_color(json["text-color"]);
+
+        lv_obj_set_style_local_text_color(label_, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, textColor);
+    }
 }
 
 void DynamicGauge::update(const JsonVariant &json)
@@ -95,18 +115,19 @@ void DynamicGauge::update(const JsonVariant &json)
     }
     else if (json.is<bool>())
     {
-        value = json.as<bool>() ? 0 : 100.0f;
+        value = json.as<bool>() ? minimum_ : maximum_;
     }
     else
     {
-        value = 0.0f;
+        value = minimum_;
     }
 
-    lv_arc_set_value(obj_, (int)(300.0f * (value / 100.0f)));
+    lv_arc_set_value(obj_, (int)(300.0f * ((value - minimum_) / (maximum_ - minimum_))));
+
     if (formating.string_format != NULL)
     {
         String labelText = String(formating.string_format);
-        labelText.replace("$$", String(value));
+        labelText.replace("$$", String(value, formating.decimal_places));
         lv_label_set_text(label_, labelText.c_str());
     }
     else
@@ -114,6 +135,14 @@ void DynamicGauge::update(const JsonVariant &json)
         String labelText = String(value,1);
         lv_label_set_text(obj_, labelText.c_str());
     }
+    lv_obj_align(label_, obj_, LV_ALIGN_CENTER, 0, 0);
+}
+
+void DynamicGauge::on_offline()
+{
+    lv_arc_set_value(obj_, 0);
+    lv_label_set_text(label_, "--");
+    lv_obj_align(label_, obj_, LV_ALIGN_CENTER, 0, 0);
 }
 
 void DynamicGauge::destroy()
